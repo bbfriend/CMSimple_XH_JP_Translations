1f5785c122e286649123ad12d4bd1d94f044630b
bbfriend <info@u-tec.org>
2016-06-15 22:33:43 +0900

    pwf_xh ckeditor

---
+++ Third_Party_plugins/ckeditor_v2.1/languages/ja.php
@@ -0,0 +1,12 @@
+<?php
+
+$plugin_tx['ckeditor']['utf-8_marker']="äöü";
+$plugin_tx['ckeditor']['toolbar']="ツールバーのサイズ";
+$plugin_tx['ckeditor']['skin']="CKeditorのデザイン";
+$plugin_tx['ckeditor']['lang']="エディタに使用される言語";
+$plugin_tx['ckeditor']['menu_main']="About...";
+$plugin_tx['ckeditor']['cf_toolbar']="ツールーバーの種類";
+$plugin_tx['ckeditor']['cf_skin']="CKeditorのスキン（デザイン）";
+$plugin_tx['ckeditor']['cf_language']="CKeditorの言語<br>・CMSimple：CMS設定で設定されてる言語（ex.ja）<br>・Browser：ブラウザから自動検出。";
+
+?>
---
+++ Third_Party_plugins/pfw_xh-0.1.0/languages/ja.php
@@ -0,0 +1,26 @@
+<?php
+
+$plugin_tx['pfw']['menu_config']="Config";
+$plugin_tx['pfw']['menu_language']="言語（翻訳）";
+$plugin_tx['pfw']['menu_stylesheet']="スタイルシート";
+$plugin_tx['pfw']['menu_help']="ヘルプ";
+$plugin_tx['pfw']['alt_logo']="Magic wand";
+$plugin_tx['pfw']['syscheck_title']="System Check";
+$plugin_tx['pfw']['syscheck_phpversion']="PHP Version ≥ %s";
+$plugin_tx['pfw']['syscheck_extension']="PHP extension %s loaded";
+$plugin_tx['pfw']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['pfw']['syscheck_xhversion']="CMSimple_XH version ≥ %s";
+$plugin_tx['pfw']['syscheck_writable']="%s is writable";
+$plugin_tx['pfw']['syscheck_alt_success']="Success";
+$plugin_tx['pfw']['syscheck_alt_warning']="Warning";
+$plugin_tx['pfw']['syscheck_alt_failure']="Failure";
+$plugin_tx['pfw']['userfunc_title']="User functions";
+$plugin_tx['pfw']['validation_minlength']="%s must be at least %d characters long!";
+$plugin_tx['pfw']['validation_maxlength']="%s must be at most %d characters long!";
+$plugin_tx['pfw']['validation_required']="%s is required!";
+$plugin_tx['pfw']['validation_pattern']="%s must match %s!";
+$plugin_tx['pfw']['validation_min']="%s must be at least %f!";
+$plugin_tx['pfw']['validation_max']="%s must be at most %f!";
+$plugin_tx['pfw']['cf_show_menu']="プラグイン管理でメニューを表示する";
+
+?>
