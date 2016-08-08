974e80247f133af45372a6e8386106d8e415d4c2
bbfriend <info@u-tec.org>
2016-08-08 22:09:01 +0900

    add hi_admin

---
+++ Third_Party_plugins/hi_admin-1.0beta1/languages/ja.php
@@ -0,0 +1,34 @@
+<?php
+
+$plugin_tx['hi_admin']['usermenu']="<li>\r\n    <span>ユーザーメニュー</span>\r\n    <ul>\r\n        <li><a href=\"#\">User1</a></li>\r\n        <li><span>XH-Links</span>\r\n            <ul>\r\n                <li><a target=\"_blank\" href=\"http://www.cmsimple-xh.org\">XH-Homepage</a></li>\r\n                <li><a target=\"_blank\" href=\"http://www.cmsimple-xh.org/wiki/doku.php/users_manual\">XH-Wiki</a></li>\r\n                <li><a target=\"_blank\" href=\"http://www.cmsimpleforum.com\">XH-Forum</a></li>\r\n            </ul>\r\n        </li>\r\n        <li><a href=\"#\">User2</a></li>\r\n        <li><a href=\"#\">User3</a></li>\r\n        <li><a href=\"#\">ここは</a>\r\n            <ul>\r\n                <li>Hi_amin→翻訳で編集</li>\r\n            </ul>\r\n        </li>\r\n    </ul>\r\n</li>";
+$plugin_tx['hi_admin']['message_xh_version']="hi_Admin は　CMSimple_XH Version 1.6 +　";
+$plugin_tx['hi_admin']['message_jQuery']="hi_Admin は jQuery4CMSimple プラグインが必要です!";
+$plugin_tx['hi_admin']['message_saved']="保存しました!";
+$plugin_tx['hi_admin']['message_not_saved']="変更は保存できませんでした。もう一度、行ってください";
+$plugin_tx['hi_admin']['message_admintemplate_not_found']="エラー：Adminテンプレート（管理者用テンプレート）が見つかりません。hi_Admin プラグインの設定を見直してください";
+$plugin_tx['hi_admin']['message_pluginmanager_deactivate']="警告：このプラグインを無効化したい時、サイトにアクセスすることができない場合は、FTP経由で設定を修正してください！";
+$plugin_tx['hi_admin']['heading_plugintable_plugins']="Plugins:";
+$plugin_tx['hi_admin']['heading_plugintable_active']="&nbsp;&nbsp;active and in admin menu:";
+$plugin_tx['hi_admin']['heading_plugintable_deactivated']="&nbsp;&nbsp;無効化:";
+$plugin_tx['hi_admin']['heading_templateeditor_template']="テンプレート編集";
+$plugin_tx['hi_admin']['heading_templateeditor_stylesheet']="スタイルシート編集";
+$plugin_tx['hi_admin']['heading_templateeditor_linklist']="テンプレートの選択:";
+$plugin_tx['hi_admin']['templateeditor_default-template']="&nbsp;[デフォルト]";
+$plugin_tx['hi_admin']['plugintable_visible']="visible";
+$plugin_tx['hi_admin']['plugintable_hidden']="hidden";
+$plugin_tx['hi_admin']['plugintable_deactivated']="無効化";
+$plugin_tx['hi_admin']['menu_main']="メイン";
+$plugin_tx['hi_admin']['cf_admintemplate_activate']=" \"Adminテンプレート（管理画面に別のテンプレート）\"を使用しますか?";
+$plugin_tx['hi_admin']['cf_admintemplate_template']="Adminテンプレートの選択";
+$plugin_tx['hi_admin']['cf_admintemplate_contenteditor']="編集画面 に、\"Adminテンプレート”を使用しますか(content.htm)?";
+$plugin_tx['hi_admin']['cf_admintemplate_pagemanager']="ページ管理 に、\"Adminテンプレート”を使用しますか?";
+$plugin_tx['hi_admin']['cf_admintemplate_filebrowser']="ファイル管理 に、\"Adminテンプレート”を使用しますか?";
+$plugin_tx['hi_admin']['cf_admintemplate_configuration']="設定の管理 に、\"Adminテンプレート”を使用しますか?";
+$plugin_tx['hi_admin']['cf_admintemplate_plugins']="プラグイン管理 に、\"Adminテンプレート”を使用しますか?";
+$plugin_tx['hi_admin']['cf_admintemplate_exclude']="\"Adminテンプレート” を適用しないプラグイン。プラグイン名を記入（複数の場合は、コンマ区切り）";
+$plugin_tx['hi_admin']['cf_templateeditor_activate']=" \"設定 > テンプレート編集\"　を有効にしますか？";
+$plugin_tx['hi_admin']['cf_templateeditor_syntaxcheck']="簡易文法チェックの使用。";
+$plugin_tx['hi_admin']['cf_pluginmanager_activate']="Do you want to use the module \"Plugin-Manager\"?";
+$plugin_tx['hi_admin']['cf_usermenu_activate']="ユーザーメニューを管理メニューに追加しますか？";
+
+?>
