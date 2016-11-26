64c4729e1d898a39fe1276ee34db94e15c171494
bbfriend <info@u-tec.org>
2016-11-27 03:17:28 +0900

    hi_admin ,etc
    hi_admin ,etc

---
+++ Third_Party_plugins/morepagedata_1.2/languages/ja.php
@@ -0,0 +1,46 @@
+<?php
+
+$plugin_tx['morepagedata']['pagedata_title'] = "More Page Data";
+$plugin_tx['morepagedata']['pagedata_tab'] = "More";
+$plugin_tx['morepagedata']['pagedata_nothing']="追加するページデータがまだ１つもありません。";
+$plugin_tx['morepagedata']['cf_path_image_folder']="Type：image_folderの画像のパス。<br>通常 <b>userfiles/images/</b> となります";
+$plugin_tx['morepagedata']['cf_slideshow_images_path']="Type：slide_showの画像のパス。 image_folder Path from CMSimple base URL to the folder, from which the images of the slide show are taken.<br/>\"<b>userfiles/images/</b>\" would usually be the path to the standard image folder.";
+$plugin_tx['morepagedata']['cf_path_template_images']="Type：template_imagesの画像のパス。<br>./templates/****/images/ を使用する場合 <b>images/</b> となります。";
+$plugin_tx['morepagedata']['cf_slideshow_use_internal_slideshow']="内部のスライドショーを使用する際にチェックしてくてください。スライドショー用のjQueryが読み込まれます。";
+$plugin_tx['morepagedata']['cf_slideshow_speed']="Time in millisec, e.g. 1000";
+$plugin_tx['morepagedata']['cf_slideshow_time-out']="Time in millisec, e.g. 1000";
+$plugin_tx['morepagedata']['cf_slideshow_default_images']="This creates a slide show also on pages, where nothing is defined in pagedata. Enter 1st the variable name (without \$), followed by = and the comma separated list of images, to be shown as default. A simple way to make such a list is to make it in the pagedata first and copy it.";
+$plugin_tx['morepagedata']['default_selection']="No selection";
+$plugin_tx['morepagedata']['error_already_morepagedata_variable']="既にmorepagedataで定義されてる変数は使用できません";
+$plugin_tx['morepagedata']['error_template_enabling_added']="Template enabling for this variable added.";
+$plugin_tx['morepagedata']['error_variable_already_used']="システムで既に使用されてる変数は、使用できません";
+$plugin_tx['morepagedata']['error_wrong_char_used']="許可されない文字が使用されてます";
+$plugin_tx['morepagedata']['field_display']="名称";
+$plugin_tx['morepagedata']['field_help_text']="Tooltip help text";
+$plugin_tx['morepagedata']['field_template']="使用するテンプレートを限定";
+$plugin_tx['morepagedata']['field_type']="Type";
+$plugin_tx['morepagedata']['field_var']="変数名";
+$plugin_tx['morepagedata']['found_former_version_mpd'] = "Found Morepagedata definitions of former version";
+$plugin_tx['morepagedata']['found_mpd_in_template'] = "Found Morepagedata definitions in template";
+$plugin_tx['morepagedata']['hint_color_picker']="Click in the input field to activate the Color Picker. <br \>Alternatively enter a 3 or 6 digit hexadecimal color code. An empty field means standard color is used.";
+$plugin_tx['morepagedata']['hint_slide_show']="Enter image file names plus a comma after each file name. To delete an image simply delete it in the input field.";
+$plugin_tx['morepagedata']['import_mpd_fields'] = "Import these definitions";
+$plugin_tx['morepagedata']['placeholder_automatic_help']="Automatic HELP TEXT is provided!";
+$plugin_tx['morepagedata']['placeholder_help_field']="Enter here HELP TEXT for input_field.";
+$plugin_tx['morepagedata']['placeholder_option_list']="OPTION1|TEXT1|OPTION2|TEXT2 ... or OPTION1||OPTION2||OPTION3";
+$plugin_tx['morepagedata']['placeholder_template_list']="Comma separated list of TEMPLATES";
+$plugin_tx['morepagedata']['select_from']="Select images by clicking from this list";
+$plugin_tx['morepagedata']['title_add']="下に追加";
+$plugin_tx['morepagedata']['title_top_var_up']="上に";
+$plugin_tx['morepagedata']['title_add_on_top']="上に追加";
+$plugin_tx['morepagedata']['title_br']="改行 (<br>) する";
+$plugin_tx['morepagedata']['title_delete']="削除";
+$plugin_tx['morepagedata']['title_help_line']="ヘルプツールチップの　表示/非表示";
+$plugin_tx['morepagedata']['title_hr']="水平線 (<hr>)を追加";
+$plugin_tx['morepagedata']['title_template_line']="テンプレート指定　の入力欄の　表示/非表示 ";
+$plugin_tx['morepagedata']['title_up']="Move variable one step up in list";
+$plugin_tx['morepagedata']['title_var_display']="編集画面のMoreタブで表示する名称";
+$plugin_tx['morepagedata']['title_var_name']="実際に使用するユニークな変数名(\$～)。後で変更することはできません、削除のみ。";
+$plugin_tx['morepagedata']['title_var_type']="変数のタイプ。変更可能";
+
+?>
