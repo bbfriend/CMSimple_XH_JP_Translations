ce71eba062b45838fb59b26fc9297716b526d409
bbfriend <info@u-tec.org>
2017-09-11 12:44:13 +0900

    add pdfviewer_XH
    pdfviewer_XH

---
+++ Third_Party_plugins/pdfviewer_XH/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$plugin_cf['pdfviewer']['start_page']="3";
+$plugin_cf['pdfviewer']['zoom']="page-width";
+$plugin_cf['pdfviewer']['pagemode']="none";
+$plugin_cf['pdfviewer']['cache']="true";
+$plugin_cf['pdfviewer']['page_refresh']="0";
+
+?>
---
+++ Third_Party_plugins/pdfviewer_XH/languages/ja.php
@@ -0,0 +1,13 @@
+<?php
+/** For ver 1.0.0  ***/
+  $plugin_tx['phpviewer']['utf8-marker']="æøå"; 
+  $plugin_tx['pdfviewer']['refresh_page']="Refresh List";
+  $plugin_tx['pdfviewer']['cf_start_page']="<p>最初のページ番号</p>";
+  $plugin_tx['pdfviewer']['cf_zoom']="<p>最初の拡大レベル</p>";
+  $plugin_tx['pdfviewer']['cf_pagemode']="<p>最初のページモード</p>";
+  $plugin_tx['pdfviewer']['cf_cache']="<p>チェックONで、ブラウザのキャッシュが利用されます。<br />
+チェックOFFにすると,userfiles/downloads/tmp にキャッシュファイルを作成。</p>
+<p> V1.0では、チェックOFFでエラーが出る場合があります(by_日本ﾁｰﾑ)";
+  $plugin_tx['pdfviewer']['cf_page_refresh']="<p>自動ページリフレッシュ間の秒数<br />
+0 =リフレッシュしない</p><p>注意！ ページの更新を有効にするには、キャッシュをオフにする必要があります。</p>"
+?>
