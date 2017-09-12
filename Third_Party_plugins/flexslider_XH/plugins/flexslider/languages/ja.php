cb8e8929253eca2db0a32c09b6166fe138bbb963
bbfriend <info@u-tec.org>
2017-09-12 18:51:13 +0900

    add Flecslider_XH Ver 0.5.4 Trans.
    Flecslider_XH Ver 0.5.4 Trans.

---
+++ Third_Party_plugins/flexslider_XH/plugins/flexslider/languages/ja.php
@@ -0,0 +1,69 @@
+<?php
+/***
+ Plugin Name: flexslider_XH_0.5.4 
+ URL:https://github.com/cmsimple-xh/flexslider 
+*****/
+$plugin_tx['flexslider']['add_item']="このスライドの後に1枚追加する";
+$plugin_tx['flexslider']['add_items']="スライドを追加する";
+$plugin_tx['flexslider']['add_n_items']=" %s枚 追加";
+$plugin_tx['flexslider']['animation_type']="アニメーション";
+$plugin_tx['flexslider']['back']="&laquo;戻る ";
+$plugin_tx['flexslider']['caption_type']="テキストの表示方法";
+$plugin_tx['flexslider']['cf_admin_autojump_to_next_image']="チェックON で管理画面でスライドファイルに画像を入力した後、次の欄にフォーカスが移動します。<br>デフォルト:チェックON";
+$plugin_tx['flexslider']['cf_admin_images_table_height']="管理画面のスライドファイルの表示サイズ。画面の小さなPCの場合、例えば 300px .<br>デフォルト:空白 600px.";
+$plugin_tx['flexslider']['cf_pagedata-imagelist_enabled']="テンプレートに flexsliderのコードを記述した場合、各ページ毎にスライダーを変更できます。チェックONにすると、各スライダーファイルの設定画面に、 ”ページ設定の\"flx_data\"を使う” のチェックが表示されます。ただし、\"flx_data\"の入力欄の作成には別プラグインの<a href=\"http://svasti.de/en/?Welcome/Morepagedata\" target=\"_blank\">Morepagedata</a>が必要となります。";
+$plugin_tx['flexslider']['cf_path_flexslider_data_files']="データの保存場所。別の保存場所が必要な場合は、パスを指定。<br>デフォルト:空白 userfiles/plugins/flexslider/ ";
+$plugin_tx['flexslider']['cf_speed_also_in_image_list']="上記の値を画像リストごとに個別に設定するか？ 個別に設定する場合は、チェックしてください";
+$plugin_tx['flexslider']['cf_speed_animation']="アニメーション時の動作のスピード(ミリ秒)。数値が低いほど早く、高いほど遅い。<br>デフォルト 1600";
+$plugin_tx['flexslider']['cf_speed_slideshow']="スライドショーでスライドする間隔のスピード(ミリ秒)。数値が低いほど早く、高いほど遅い。<br>デフォルト 5000";
+$plugin_tx['flexslider']['cf_textelement_marginhorizontal']="テキストのmargin-right/left 初期値 % <br>デフォルト:10";
+$plugin_tx['flexslider']['cf_textelement_margintop']="テキストのmargin-top 初期値 % <br>デフォルト: 5";
+$plugin_tx['flexslider']['changeto_fullsize']="Enlarge image to full screen size";
+$plugin_tx['flexslider']['changeto_smallsize']="Go back to smaller image";
+$plugin_tx['flexslider']['close']="閉じる";
+$plugin_tx['flexslider']['delete_item']="このスライドを削除";
+$plugin_tx['flexslider']['file-manager_copy_file']="コピー";
+$plugin_tx['flexslider']['file-manager_create_new_file']="新規作成";
+$plugin_tx['flexslider']['file-manager_delete_file']="削除";
+$plugin_tx['flexslider']['file-manager_enter_new_file_name']="新規ファイル名(半角英数字)";
+$plugin_tx['flexslider']['file-manager_select']="スライダーファイルの作成・編集";
+$plugin_tx['flexslider']['file-manager_standard_file']="ファイル名無しのデフォルト用";
+$plugin_tx['flexslider']['file-manager_wrong_char']="<b>エラー:</b> 半角英数字のみです。 <b>a-z A-Z _ - . 0-9</b>.";
+$plugin_tx['flexslider']['fill_from_folder']='フォルダごと使う';
+$plugin_tx['flexslider']['fill_from_folder_title']='上記のフォルダから、すべての画像をリストに入れる.サブフォルダは適用されません';
+$plugin_tx['flexslider']['halt_on_mouseover']='マウスオーバーで停止';
+$plugin_tx['flexslider']['height']="高さ";
+$plugin_tx['flexslider']['image_editing']="サイズ変更";
+$plugin_tx['flexslider']['image_text']="テキスト";
+$plugin_tx['flexslider']['imagebrowser_usage_hint']="フォルダを選択してスライダーに使用する画像をダブルクリックで指定してください。";
+$plugin_tx['flexslider']['items']="枚";
+$plugin_tx['flexslider']['link']="リンク";
+$plugin_tx['flexslider']['link_enter_external']="他サイトのURL等";
+$plugin_tx['flexslider']['link_external']="他サイトへリンク";
+$plugin_tx['flexslider']['link_no']="リンクなし";
+$plugin_tx['flexslider']['list_saved']="Flexsliderリストを保存しました。";
+$plugin_tx['flexslider']['move_down']="下に";
+$plugin_tx['flexslider']['move_up']="上に";
+$plugin_tx['flexslider']['nav_control']="ナビゲーション";
+$plugin_tx['flexslider']['nav_control_title']="ナビゲーション &bull;&bull;&bull; を表示";
+$plugin_tx['flexslider']['nav_direction']="前次ボタン";
+$plugin_tx['flexslider']['nav_direction_title']=" 前の < と 次の > を表示";
+$plugin_tx['flexslider']['new_size']="サイズ変更";
+$plugin_tx['flexslider']['pdata_img_control']="各ページの\"flx_data\"を使う";
+$plugin_tx['flexslider']['pdata_title']="チェックすると各ページの flx_data をスライドのデータとして使います。テンプレートにFlexSliderのコードがある場合に有効";
+$plugin_tx['flexslider']['preview']="プレビュー";
+$plugin_tx['flexslider']['random']="ランダム表示";
+$plugin_tx['flexslider']['randomstart']="ランダムスタート";
+$plugin_tx['flexslider']['remove_from_list']="このスライドから画像だけ削除する";
+$plugin_tx['flexslider']['return']="Return";
+$plugin_tx['flexslider']['save_as']="保存";
+$plugin_tx['flexslider']['slider_size']="表示サイズ";
+$plugin_tx['flexslider']['slider_size_title']="横：縦の比率。画像のサイズが固定されていない場合に試みてください。最も高いスライドがこの長方形のサイズを決定し、背の低い画像は空白が多くなります。";
+$plugin_tx['flexslider']['speed_animation']="アニメーション速度(ms)";
+$plugin_tx['flexslider']['speed_animation_title']="アニメーション速度(ms),未設定でデフォルト値： %s msecとなります ";
+$plugin_tx['flexslider']['speed_slideshow']="スライドショー速度(ms)";
+$plugin_tx['flexslider']['speed_slideshow_title']="スライドショー速度(ms), 未設定でデフォルト値： %s msecとなります";
+$plugin_tx['flexslider']['text_only']="テキストのマージン";
+$plugin_tx['flexslider']['width']="幅";
+
+?>
